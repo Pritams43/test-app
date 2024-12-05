7c59f06a02db7dc9f1e6c60cc7c1f710ef0f537d
pritams43 <pritam.shah@tekditechnologies.com>
2024-12-05 15:08:58 +0530

    code-changes

--- src/App.js
+++ src/App.js
@@ -7,14 +7,14 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Gitworkflow<code>src/App.js</code> and save to reload.
+          Testing Git CICD worflow 
         </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
+        > 
           Learn React
         </a>
       </header>
